acc901e8347f5a83184ddcab3cf7d48948a3bc92
Giovanni <gio.reder@gmail.com>
2024-05-21 16:33:37 +0200

    update

--- src/pratica/es3/Es3_while.java
+++ src/pratica/es3/Es3_while.java
@@ -5,15 +5,31 @@ import java.util.Scanner;
 public class Es3_while {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Scrivi una stringa");
-
-        String stringa = scanner.nextLine();
-        String[] words = stringa.split("");
-        String addVirgola = String.join(",", words);
+        String input;
 
 
-        while (stringa.contains(":q"))
-            System.out.println(addVirgola);
+        while (true) {
+            System.out.print("Inserisci una stringa o ':q' per uscire: ");
+            input = scanner.nextLine();
+
+
+            if (input.equals(":q")) {
+                System.out.println("Uscita dal programma.");
+                break;
+            }
+
+
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < input.length(); i++) {
+                result.append(input.charAt(i));
+                if (i < input.length() - 1) {
+                    result.append(", ");
+                }
+            }
+
+
+            System.out.println("Stringa suddivisa: " + result.toString());
+        }
 
         scanner.close();
     }
