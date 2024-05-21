7be697c819bdd48818aeeb5b566db850e3020f1e
Giovanni <gio.reder@gmail.com>
2024-05-21 16:51:25 +0200

    done

--- src/pratica/es3/Es3_while.java
+++ src/pratica/es3/Es3_while.java
@@ -5,35 +5,37 @@ import java.util.Scanner;
 public class Es3_while {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String input;
-
 
         while (true) {
-            System.out.print("Inserisci una stringa o ':q' per uscire: ");
-            input = scanner.nextLine();
+            System.out.print("Scrivi una stringa o ':q' per uscire: ");
+            String testo =scanner.nextLine();
 
-
-            if (input.equals(":q")) {
-                System.out.println("Uscita dal programma.");
+            if (testo.equals(":q")) {
+                System.out.println("Hai inserito la stringa :q, fine del ciclo while");
                 break;
             }
 
-
             StringBuilder result = new StringBuilder();
-            for (int i = 0; i < input.length(); i++) {
-                result.append(input.charAt(i));
-                if (i < input.length() - 1) {
+            for (int i = 0; i < testo.length(); i++) {
+                result.append(testo.charAt(i));
+                if (i < testo.length()) {
                     result.append(", ");
                 }
             }
 
 
-            System.out.println("Stringa suddivisa: " + result.toString());
+            System.out.println( result);
         }
 
         scanner.close();
     }
 }
+
+
+
+
+
+
 
 /* Chiede in ingresso una stringa
 * la stringa viene suddivisa in caratteri singoli separati da una virgola
